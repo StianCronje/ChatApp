3c988bad21bc652efd6c6f25d309553e2456a392
Stian Cronje <scronje@live.com>
2018-03-10 01:32:09 -0600

    Project Init
    some basic MVVM

---
+++ FormsChatApp/FormsChatApp/FormsChatApp/ViewModels/ChatViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FormsChatApp.Annotations;
+using Xamarin.Forms;
+
+namespace FormsChatApp.ViewModels
+{
+    public class ChatViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _message;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                if (value == _message) return;
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command TestCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    Message = "Testing: " + Name;
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
