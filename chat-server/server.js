b34ffdcc8b85dcdbd29cb0a62aac57a32d27c331
Stian Cronje <scronje@live.com>
2018-03-12 13:54:12 -0500

    server v001

--- chat-server/server.js
+++ chat-server/server.js
@@ -2,7 +2,6 @@ const mongo = require('mongodb').MongoClient;
 const io = require('socket.io').listen(4000).sockets;
 
 const url = 'mongodb://localhost/chat';
-const dbname = 'chat';
 
 //Connect to DB
 mongo.connect(url, function(err, db){
@@ -11,4 +10,42 @@ mongo.connect(url, function(err, db){
     }
 
     console.log('MongoDB Connected...')
+
+    //Connect to Socket.io
+    io.on('connection', function(socket){
+        let chat = db.collection('chats');
+
+        // send status message
+        sendStatus = function(s){
+            socket.emit('status', s);
+        }
+
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
+            }
+
+            // emit messages
+            socket.emit('output', res);
+        });
+
+        // Handle input events
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+
+            if(name == '' || message == ''){
+                sendStatus('Please enter a name and message');
+            } else {
+                chat.insert({name: name, message: message}, function(){
+                    io.emit('output', [data]);
+
+                    sendStatus({
+                        message: 'Message sent',
+                        clear: true
+                    })
+                });
+            }
+        });
+    })
 });
